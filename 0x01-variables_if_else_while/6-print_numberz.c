02f11402fb6e8959472e647bf166b261e49428cb
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-18 12:53:13 -0700

    6-print_numberz.c commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0) (success)
+ */
+
+	int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 10; ch++)
+
+	putchar(ch);
+	return (0);
+}
