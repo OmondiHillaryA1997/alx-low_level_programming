7195aee132661ab20cdf0fd23444a3de2731f34f
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-19 12:09:59 -0700

    8-print_base16.c commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0) (success)
+ */
+
+	int main(void)
+{
+		int b;
+	for (b = 0x0; b < 0x16; b++)
+	{
+		putchar(%x:);
+	}
+	putchar('\n');
+	return (0);
+}
