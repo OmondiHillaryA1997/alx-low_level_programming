9945c0e1c8fabe649e0e5f9390490f2a8249409b
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-21 14:44:15 -0700

    1-alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A program that prints the alphabet and a newline
+ * Return: always (0) (success)
+*/
+
+	void print_alphabet(void)
+{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+}
