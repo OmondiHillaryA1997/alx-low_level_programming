9d6132c53c39b752c616069283173d2b1ee24224
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-22 16:10:58 -0700

    101-natural.c commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,8 +11,10 @@
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum + = i;
+		if ((i % 5 == 0) || (i % 3 == 0))
+		{
+			sum = sum + 1;
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
