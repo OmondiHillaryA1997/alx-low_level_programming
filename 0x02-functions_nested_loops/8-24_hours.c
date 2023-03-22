c322efd8cf7c508e78fb7360c706542acdf936b0
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-22 14:28:54 -0700

    8-24_hours.c commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * jack baeur - func is to print every min of the day of jack baeur, starti
+ * jack baeur - function is to print every min of the day of jack baeur, starti
  * n from 00:00 to 23:59, min loop counts min, while hour loop counts hours
  * and resets mins
  * Return: 0
  */
-
-	void jack baeur(void)
+void jack_bauer(void)
 {
 	int hours = 0;
 	int mins = 0;
@@ -15,20 +14,20 @@
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59)
+		while (mins <= 59)
 		{
-			minutes_reminder = minutes % 10;
-			hours_reminder = minutes % 10;
+			mins_reminder = mins % 10;
+			hours_reminder = mins % 10;
 			_putchar(hours / 10 + '0');
 			_putchar(hours_reminder + '0');
 			_putchar(' : ');
 				_putchar(mins / 10 + '0');
-			_putchar(minutes_reminder + '0');
-			minutes++;
+			_putchar(mins_reminder + '0');
+			mins++;
 			_putchar('\n');
 
 			hours++;
-			minutes = 0;
+			mins = 0;
 		}
 	}
 }
