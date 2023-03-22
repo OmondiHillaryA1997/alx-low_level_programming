c922135311784a3695a28a239bfd07faea0078db
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-22 12:59:17 -0700

    4-isalpha.c commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isaplha - func is to check if c is a letter, lowercase or uppercase
+ * _isalpha - func is to check if c is a letter, lowercase or uppercase
  * @c: is the argument that will be used
  * Return: 0
  */
-int _isaplha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
