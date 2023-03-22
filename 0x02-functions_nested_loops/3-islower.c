4c0fd25ee418ce527e3ac0f019e674f09f6bc09b
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-22 10:33:56 -0700

    3-islower.c commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - A function tha checks for lowercase character
+ * @c - parameter that is used to check fir lowercase
+ * Return: (0) (success)
+*/
+
+	int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		else
+	return (0);
+}
