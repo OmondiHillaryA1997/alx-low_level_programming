67fc326bf3dd66f08e8a95f691674c60893a6ca6
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-22 11:15:46 -0700

    2-print_alphabet_x10.c commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - A function tha checks for lowercase character
- * @c - parameter that is used to check fir lowercase
+ * @c - parameter that is used to check for lowercase
  * Return: (0) (success)
 */
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
-
 int _putchar(char c);
 void print_alphabet(void);
-#endif
+void print_alphabet_x10(void);
+int _islower(int c);
