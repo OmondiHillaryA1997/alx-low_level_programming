72be326b4b73bdb9d0cc3831321d96c7747da276
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-23 16:00:24 -0700

    5-more_numbers.c commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,15 @@
  * print_most_numbers - function to print 0 t0 9 with an exception of 2 and 4
  * Return: null
 */
-	void print_most_numbers(void)
+void print_most_numbers(void)
 {
-		char c;
+		int c;
 
-		for (c = '0'; c <= '9'; c++)
+		for (c = 0; c <= 9; c++)
 		{
-			if (!(c == '2' || c == '4'))
-				_putchar(c);
+			if (c == 2 || c == 4)
+				continue;
+			_putchar(c + 48);
 		}
 		_putchar('\n');
 }
