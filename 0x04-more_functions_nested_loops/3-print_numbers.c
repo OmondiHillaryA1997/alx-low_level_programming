ed0749db892ddcc8cf43c62e1c34bc534c243d43
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-23 13:06:35 -0700

    3-print_numbers.c commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,11 @@
 
 void print_numbers(void)
 {
-int n;
+	char c;
 
-for (n = 0; n <= 9; n++)
-_putchar(n +'0');
-_putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
