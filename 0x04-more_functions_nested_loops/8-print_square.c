2c163501e48c8eac4fb29be13d130e6174bc5f8e
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-24 00:34:36 -0700

    8-print_square.c commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,20 @@
 
 void print_square(int size)
 {
-if (size <= 0)
-{
-_putchar('\n');
-} else
-{
-int x, y;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int x, y;
 
-for (x = 0; x < size; x++)
-{
-for (y = 0; y < size; y++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
