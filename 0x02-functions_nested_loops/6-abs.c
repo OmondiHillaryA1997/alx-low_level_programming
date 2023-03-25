016f8aed7bc176b2c8688186b86e01dabe159145
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-25 01:09:19 -0700

    6-abs.c commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,15 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _abs - func is to check if c is a letter, lowercase or uppercase
- * @c: is the argument that will be used
- * Return: 0
+ * _abs - function that computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
  */
-	int _abs(int c)
+int _abs(int n)
 {
-	if (c < 0 || c == 0)
+	if (n >= 0)
 	{
-		return (c);
+		return (n);
 	}
-	else
-		return (c * -1);
+	return (-n);
 }
