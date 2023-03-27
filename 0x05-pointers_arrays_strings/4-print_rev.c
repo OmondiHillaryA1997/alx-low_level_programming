e8dc6551fdb238ed552d42d5833c2f4126ba0f10
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-27 12:50:04 -0700

    4-print_rev.c commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,20 @@
 
 void print_rev(char *s)
 {
+	int count = 0;
+	int p;
+
 	while (*s == '\0')
 	{
+		count++;
+		s++;
+	}
+	s--;
+	for (p = count; p > 0; p--)
+	{
 
-		_putchar(*s--);
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }
