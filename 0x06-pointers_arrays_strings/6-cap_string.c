378f0992e5a7065627b7f1c0b6458a7bcb835dd3
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-03-30 13:27:12 -0700

    6-cap_string.c commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - Function that capitalizes all words of a string
+ * @s: input string
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] < '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		putchar(*s);
+		putchar(',');
+		putchar(';');
+		putchar('.');
+		putchar('!');
+		putchar('?');
+		putchar('(');
+		putchar('"');
+		putchar('(');
+		putchar(')');
+		putchar('{');
+		putchar('}');
+	}
+	putchar('\n');
+	return (s);
+}
