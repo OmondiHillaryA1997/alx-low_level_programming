010831fff38c1c208704e13821dddfc25de6e088
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-04-03 14:14:41 -0700

    4-strpbrk.c commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function that searches for any set of bytes
+ * @s: first string
+ * @accept: second string
+ * Return: returns a pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int count;
+	int a;
+
+	for (count = 0; s[count]; count++)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (s[count] == accept[a])
+			{
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}
