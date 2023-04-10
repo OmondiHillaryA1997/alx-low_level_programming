3723afbfc74f0894786c9ce67ed3a67a3de9ae48
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-04-10 11:06:50 -0700

    4-add.c commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Function that adds positive numbers
+ * @argc: counts arguments in functions
+ * @argv: argument that point on strings
+ * Return: (0) (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int sum = 0;
+
+	for (count = 1; count < argc; count++)
+	{
+		if (argc == 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		else
+		{
+			if (!atoi(argv[count]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum = sum + atoi(argv[count]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
