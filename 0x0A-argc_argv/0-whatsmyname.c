2c93d5766996cc846b538151ad269cc7c60efc1b
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-04-09 20:43:44 -0700

    0-whatsmyname.c commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,10 @@
  * Return: (0) (success)
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",*argv);
+	printf("%s\n", *argv);
 
 	return (0);
 }
