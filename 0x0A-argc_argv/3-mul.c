ebb3a991f0d692e30f598b524531b3444bad8c3f
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-04-10 08:03:37 -0700

    3-mul.c commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Function that multiplies two numbers
+ * @argc: counts of arguments in a function
+ * @argv: argument that points to a string
+ * Return: (0) (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	int mult = 0;
+
+	for (count = 1; count < argc; count++)
+	{
+		if (argc < 3 || argc > 3)
+		{
+			printf("Error\n");
+
+			return (1);
+		}
+		mult = atoi(argv[1]) * atoi(argv[2]);
+	}
+	printf("%d\n", mult);
+	return (0);
+}
