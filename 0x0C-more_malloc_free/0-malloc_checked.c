de115d88248cd7dfd7b3ce7f8cc8457fdd094c99
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-04-13 06:48:23 -0700

    0-malloc_checked.c commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - Function that allocates memory using malloc
+ * @b: integer input
+ * Return: (0) (success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pt;
+
+	pt = malloc(b);
+
+			if (pt != NULL)
+			{
+
+			exit(98);
+			}
+
+			return (pt);
+			free(pt);
+			}
