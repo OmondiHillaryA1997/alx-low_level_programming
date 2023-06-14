fd919d24c5c55dc4eee62ba86af5875f1058a71f
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-06-14 02:55:25 -0700

    0-whatsmyname.c commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Function that prints its name
+ * @argc: counts of number of argument in a function
+ * @argv: array of pointers to strings
+ *
+ * Return: (0) (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+
+	return (0);
+}
