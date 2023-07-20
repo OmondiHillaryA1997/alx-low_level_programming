cca77800c45f31d28bde99d4c9705ce0693a4b07
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-07-19 20:04:07 -0700

    2-int_index.c commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer
+ * @array: string parameter
+ * @size: size parameter
+ * @cmp: Funtion pointer
+ * Return: NULL
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	if (cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+
+		if (cmp(array[i]))
+			return (-1);
+	}
+	return (-1);
+}
