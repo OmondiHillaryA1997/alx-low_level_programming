89b64d5ab5805b9e60e5ea4549f952a7b113367a
OmondiHillaryA@1997 <omondihillary1997@gmail.com>
2023-07-27 05:58:54 -0700

    Linked first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - Function that prints all elements in list_t
+ * @h: header pointer
+ *
+ * Return: integer
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t p;
+
+	p = 0;
+
+	for (; h != NULL; p++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%u]%p\n", h->len, h->str);
+		h = h->next;
+	}
+	return (p);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+0x12 - C - SINGLY LINKED LISTS
+- Printing all elements of a list_t list
+- Returning the numbers pf elements in a linked list_t list
+- Adding a new node at the beginning of a list_t list
+- Adding new node at the end of a list_t list
+- Freeing a list_t list
