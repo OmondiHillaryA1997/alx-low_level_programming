6a1f04e81ca58876a60323a5c3ffbf260007c06f
OmondiHillaryA@1997 <omondihillary1997@gmail.com>
2023-07-27 08:15:58 -0700

    add node commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add_node - Function that adds a new node at the beginning of a list
+ * @head: pointer to first node
+ * @str: insertion argument
+ *
+ * Return: integer
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *insert;
+	unsigned int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+
+		insert = malloc(sizeof(list_t));
+	if (!insert)
+		return (NULL);
+
+	insert->str = strdup(str);
+
+	insert->len = len;
+	insert->next = (*head);
+	(*head) = insert;
+
+	return (*head);
+}
