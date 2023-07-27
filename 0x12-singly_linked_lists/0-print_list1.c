e168df53516825c39479791c4a34c587d2dffde0
OmondiHillaryA@1997 <omondihillary1997@gmail.com>
2023-07-27 06:04:27 -0700

    Linked first commit

--- 0x12-singly_linked_lists/0-print_list1.c
+++ 0x12-singly_linked_lists/0-print_list1.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -20,7 +19,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 
 		else
-			printf("[%u]%s\n", h->len, h->str);
+			printf("[%u]%p\n", h->len, h->str);
 		h = h->next;
 	}
 	return (p);
