482da78599eb1819a34b2336fd86a122daa6aa98
OmondiHillaryA@1997 <omondihillary1997@gmail.com>
2023-07-27 06:18:45 -0700

    Linked first commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,10 +20,14 @@ size_t print_list(const list_t *h)
 	for (; h != NULL; p++)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 
 		else
+		{
 			printf("[%d]%s\n", h->len, h->str);
+		}
 		h = h->next;
 	}
 	return (p);
