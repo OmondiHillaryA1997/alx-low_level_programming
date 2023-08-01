994cb783eb381e99a27ea7efdc4e62a66836eca7
OmondiHillaryA@1997 <omondihillary1997@gmail.com>
2023-08-01 16:12:13 -0700

    9-insert_nodeint.c commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,8 +24,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = tmp->next;
-		tmp->next = new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 
