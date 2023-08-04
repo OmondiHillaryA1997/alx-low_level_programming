318a4db1031c98a0cee124e57b438e2e5468d6e4
OmondiHillaryA@1997 <omondihillary1997@gmail.com>
2023-08-04 13:39:09 -0700

    1-print_binary.c commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - Function that prints binary representation of a number
+ * @n: unsigned integer
+ *
+ * Return: integer
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int result;
+	int p;
+
+	result = 0;
+	while (result != 0)
+	{
+		if ((n & result == 0)
+		{
+			if (p)
+				_putchar (')');
+		}
+		else
+		{
+			_putchar ('1');
+			p = 1;
+		}
+		result >>= 1;
+	}
+
+	if (!p)
+		_putchar ('0');
+
+	_putchar ('\n');
+}
