521c70d1172bf113edf3ef63e144771b55e9c7aa
OmondiHillaryA1997 <omondihillary1997@gmail.com>
2023-09-04 06:55:11 -0700

    1-create_file.c commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		length = 0;
 		while (text_content[length])
@@ -25,7 +25,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	c = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (c == -1);
 	y = write(c, text_content, length);
 
 	while (c == -1 || y == -1)
